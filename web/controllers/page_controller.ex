2d1a18a604aa94b53c5fc3a76d16e8bbd43a185b
kepung <kepung@gmail.com>
2017-08-01 22:40:02 +1200

    converting models to json

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,31 @@
 defmodule HelpOn.PageController do
   use HelpOn.Web, :controller
+   
+   
+  def index(conn, _params) do 
 
-  def index(conn, _params) do
-    render conn, "index.html"
+    alias HelpOn.User
+    import Ecto.Query
+    import Poison
+
+    users = HelpOn.Repo.all(from u in User, select: u.name)
+
+    #conn  |> put_flash(:info, "Welcome to Phoenix, from flash info!")
+    #|> put_flash(:error, "Let's pretend we have an error.")
+    #|> render("index.html")
+    #render conn, "index.html"
+
+    #IO.inspect conn.req_headers
+    #IO.inspect conn
+
+    #users = HelpOn.Repo.all(User)
+
+    #render conn, "index.html", users: users
+    #render conn, users: users
+    json conn, to_string Poison.Encoder.encode(users, [])
+
+    #json conn, %{id: 1}
+
+
   end
 end
