eeb57235e33165421cd541ac77efe88880ccf04b
kepung <kepung@gmail.com>
2017-08-05 19:28:25 +1200

    Ectohelper helps to serialized data.

--- web/controllers/request_controller..ex
+++ web/controllers/request_controller..ex
@@ -3,21 +3,26 @@ defmodule HelpOn.RequestController do
 
   alias HelpOn.RequestProvider
   alias HelpOn.User
+  
 
   def handleRequest(conn, %{"type" => requestType, "id" => id }) do 
     import Poison
     import Ecto.Query
 
-    users = HelpOn.Repo.all(from u in User, select: { u.name, u.email})
-    IO.inspect users
+    #users = HelpOn.Repo.all(from u in User, select: { u.name, u.email})
+    #IO.inspect users
 
     #render conn, "request.html", %{requestType: requestType, id: id}
     element = Integer.parse(id)
-    dataRecord = RequestProvider.getAllService()
+    dataRecord = RequestProvider.getService(elem(element, 0))
+    data = HelpOn.EctoHelper.strip_meta(dataRecord)
+
+    IO.inspect data
 
     if dataRecord != nil do
+      Poison.encode!(data)
       #json conn, to_string Poison.Encoder.encode(dataRecord, [])
-      json conn,  %{id: 1}
+      json conn,  data
     end
     json conn, %{id: 1}
   end  
